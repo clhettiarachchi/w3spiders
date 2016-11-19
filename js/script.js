23c0c022709b9a342287cb83ed1223aa436f3bca
clhettiarachchi <chandikalakshan@gmail.com>
2016-11-20 04:15:53 +0530

    commit message

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+	    $('#whyus .item > .bgwrapper').matchHeight();
+	    $('#whyus .item > .bgwrapper').addClass('margin-bottom');
+	    $('#slider .carousel-inner .item h2').addClass('animated zoomIn');
+	    $('#slider .carousel-inner .item p').addClass('animated slideInUp');
+	
+
+});
+
+AOS.init({
+  	duration: 1200, disable: 'mobile'
+});
+
