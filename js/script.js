826196169f6f5f8c63355f85dee5c339d91c32e1
clhettiarachchi <chandikalakshan@gmail.com>
2016-11-25 21:41:04 +0530

    commit message

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 $(function() {
-	    $('#whyus .item > .bgwrapper').matchHeight();
 	    $('#whyus .item > .bgwrapper').addClass('margin-bottom');
 	    $('#slider .carousel-inner .item h2').addClass('animated zoomIn');
 	    $('#slider .carousel-inner .item p').addClass('animated slideInUp');
-	
 
 });
 
